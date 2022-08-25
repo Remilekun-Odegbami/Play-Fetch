ac68ed51352f04111bdede4238428a6dcca40659
Remilekun-Odegbami <69697219+Remilekun-Odegbami@users.noreply.github.com>
2022-08-25 12:04:04 +0100

    play catch initial commit

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // this destroys the dog
+        Destroy(gameObject);
+        // destroys the ball when the dog hits it
+        Destroy(other.gameObject);
+    }
+}
+
+
---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    bool readyToInstantiate;
+
+    void Start()
+    {
+        readyToInstantiate = true;
+    }
+
+    void Update()
+    {
+        if (readyToInstantiate && Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(PreventSpam());
+
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+
+    IEnumerator PreventSpam()
+    {
+        // this stops the users from shooting multiple dogs at once 
+        readyToInstantiate = false;
+        // the wait time
+        yield return new WaitForSeconds(1);
+        // this enables the user shoot another dog after 1 second
+        readyToInstantiate = true;
+    }
+
+}
